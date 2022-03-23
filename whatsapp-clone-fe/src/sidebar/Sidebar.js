e9cd45c9a85942b0487658b381b62f5dbf8dd450
Claudio <claudio2000@hotmail.it>
2022-03-23 10:49:24 +0100

    Chiusura sidebar statica

--- whatsapp-clone-fe/src/sidebar/Sidebar.js
+++ whatsapp-clone-fe/src/sidebar/Sidebar.js
@@ -3,14 +3,18 @@ import './Sidebar.css'
 import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert'
 import DonutLargeIcon from '@mui/icons-material/DonutLarge'
+import SearchIcon from '@mui/icons-material/Search';
 import {Avatar, IconButton } from '@mui/material';
+import SidebarChat from './SidebarChat';
 
 const Sidebar = () =>{
     return(
         <div className='Sidebar'>
             <div className='sidebarHeader'>
                 <div className='sidebarHeaderLeft'>
-                    <Avatar src="https://www.sapereambiente.it/wp-content/uploads/2020/05/Bradipo-gigante.jpeg"/>
+                    <IconButton>
+                        <Avatar src="https://www.sapereambiente.it/wp-content/uploads/2020/05/Bradipo-gigante.jpeg"/>
+                    </IconButton>
                 </div>
                 <div className='sidebarHeaderRight'> 
                     <IconButton>
@@ -24,6 +28,17 @@ const Sidebar = () =>{
                     </IconButton>
                     
                 </div>
+            </div>
+            <div className='sidebarSearch'>
+                <div className='sidebarSearchContainer'>
+                    <SearchIcon/>
+                    <input type="text" placeholder='Cerca o inizia una nuova chat'/>
+                </div>
+            </div>
+            <div className='sidebarChatBody'>
+                <SidebarChat valore="1"/>
+                <SidebarChat valore="2"/>
+                <SidebarChat valore="3"/>
             </div>
         </div>
     )
