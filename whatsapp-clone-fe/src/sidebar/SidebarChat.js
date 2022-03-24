c4096bce64b7cc2460808c05a1ddc6e0a468bd46
Donny2stroke <claudio2000@hotmail.it>
2022-03-24 18:20:29 +0100

    Fix ultimo messaggio (da completare)

--- whatsapp-clone-fe/src/sidebar/SidebarChat.js
+++ whatsapp-clone-fe/src/sidebar/SidebarChat.js
@@ -7,7 +7,16 @@ import { Link } from 'react-router-dom';
 const SidebarChat = ({room}) =>{
 
     const [seed, setSeed] = useState("")
+    const [ultimoMessaggio, setUltimoMessaggio] = useState("")
 
+    useEffect(()=>{
+        let ultimoMessaggio = (room?.messages 
+            && room.messages[room.messages.length -1]
+            && room.messages[room.messages.length -1].message || "-")
+        let messaggioTroncato = ultimoMessaggio.length > 5 ? `${ultimoMessaggio.substr(0,5)}...` : ultimoMessaggio
+        setUltimoMessaggio(messaggioTroncato)
+    },[])
+    
     useEffect(()=>{
         setSeed(Math.floor(Math.random()*5000))
     },[])
@@ -19,7 +28,7 @@ const SidebarChat = ({room}) =>{
                 <Avatar src={`https://avatars.dicebear.com/api/adventurer/${seed}.svg`}/>
                 <div className='sidebarChatInfo'>
                     <h2>Nome: {room.name}</h2>
-                    <p>Messaggio</p>
+                    <p>{ultimoMessaggio}</p>
                 </div>
             </div>
         </div>
